fbc057d6a7b9d9f1ed1d5cee61e5fca8b8c0714b
Martin <Martinhanyfcis@gmail.com>
2025-03-03 01:27:00 +0200

    Columnar Encryption Function Done

--- SecurityPackage/SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
+++ SecurityPackage/SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
@@ -20,7 +20,41 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int columns = key.Count;
+            plainText = plainText.Replace(" ", "");
+            int rows = (int)Math.Ceiling((double)plainText.Length / columns);
+            char[,] arr = new char[rows, columns];
+            int index = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (index < plainText.Length)
+                    {
+                        arr[i, j] = plainText[index++];
+                    }
+                    else
+                        arr[i, j] = 'X';
+                }
+            }
+            List<KeyValuePair<int, int>> keyWthIndex = new List<KeyValuePair<int, int>>();
+            int idx = 0;
+            foreach (int k in key)
+            {
+                keyWthIndex.Add(new KeyValuePair<int, int>(k, idx++));
+            }
+            keyWthIndex = keyWthIndex.OrderBy(pair => pair.Key).ToList();
+            string cipherText = "";
+            foreach (var pair in keyWthIndex)
+            {
+                int colIndex = pair.Value;
+                for (int i = 0; i < rows; i++)
+                {
+                    cipherText += arr[i, colIndex];
+                }
+            }
+            return cipherText.ToUpper();
         }
     }
 }
