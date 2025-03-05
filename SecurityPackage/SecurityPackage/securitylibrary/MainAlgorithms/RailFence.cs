774e10133db1b289c98e506939c75d462adba63f
kamal-bahaa <kamalbahaa53z@gmail.com>
2025-03-05 20:52:02 +0200

    railfence

--- SecurityPackage/SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
+++ SecurityPackage/SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,81 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            for (int key = 1; key <= plainText.Length; key++)
+            {
+                string s = Encrypt(plainText, key);
+                if (s.Equals(cipherText, StringComparison.InvariantCultureIgnoreCase))
+                    return key;
+            }
+            return -1;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            int n = cipherText.Length, c, idx = 0;
+            string[] newText = new string[key];
+
+            int remain = n % key;
+
+            for (int i = 0; i < key; i++)
+            {
+                c = n / key;
+                if (remain != 0)
+                {
+                    c += 1;
+                    remain--;
+                }
+                newText[i] = cipherText.Substring(idx, c);
+                idx += c;
+            }
+
+            string plainText = "", firstString = newText[0];
+
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                for (int j = 0; j < key; j++)
+                {
+                    string s = newText[j];
+                    if (newText[j] != null && i < s.Length)
+                    {
+                        plainText += newText[j][i];
+                    }
+                }
+            }
+            return plainText;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            int n = plainText.Length;
+
+            if (key <= 1)
+                return plainText;
+
+            plainText = plainText.Replace(" ", "");
+            plainText = plainText.ToUpper();
+
+            string[] newText = new string[key];
+
+            int pos = 0, end = key;
+
+            foreach (char c in plainText)
+            {
+                newText[pos++] += c;
+                if (pos == end)
+                    pos = 0;
+            }
+
+            string cipher = "";
+
+            for (int i = 0; i < key; i++)
+            {
+                cipher += newText[i];
+            }
+
+            return cipher;
+
         }
     }
 }
+
