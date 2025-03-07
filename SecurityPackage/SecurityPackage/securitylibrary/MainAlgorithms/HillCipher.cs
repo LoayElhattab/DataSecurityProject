1d5ddfbf90227c342a9680e320592023cb5c67f3
Mugetsu276 <loayelhattab@yahoo.com>
2025-03-07 14:22:13 +0200

    Finished Hill cipher 3*3 Analysis

--- SecurityPackage/SecurityPackage/securitylibrary/MainAlgorithms/HillCipher.cs
+++ SecurityPackage/SecurityPackage/securitylibrary/MainAlgorithms/HillCipher.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -48,6 +49,7 @@ namespace SecurityLibrary
                 }
                 if (flag) break;
             }
+            if (!flag) throw new InvalidAnlysisException();
             plainTextInverse[3] = plainText[i];
             plainTextInverse[0] = plainText[j + 1];
             plainTextInverse[1] = -1 * plainText[i + 1];
@@ -64,10 +66,10 @@ namespace SecurityLibrary
             }
             //Key Calculation
             List<int> key = new List<int>(new int[4]);
-            key[0] = (cipherTextEquiv[0] * plainTextInverse[0] + cipherTextEquiv[2] * plainTextInverse[1]) % 26;
-            key[1] = (cipherTextEquiv[0] * plainTextInverse[2] + cipherTextEquiv[2] * plainTextInverse[3]) % 26;
-            key[2] = (cipherTextEquiv[1] * plainTextInverse[0] + cipherTextEquiv[3] * plainTextInverse[1]) % 26;
-            key[3] = (cipherTextEquiv[1] * plainTextInverse[2] + cipherTextEquiv[3] * plainTextInverse[3]) % 26;
+            key[0] = cipherTextEquiv[0] * plainTextInverse[0] + cipherTextEquiv[2] * plainTextInverse[1];
+            key[1] = cipherTextEquiv[0] * plainTextInverse[2] + cipherTextEquiv[2] * plainTextInverse[3];
+            key[2] = cipherTextEquiv[1] * plainTextInverse[0] + cipherTextEquiv[3] * plainTextInverse[1];
+            key[3] = cipherTextEquiv[1] * plainTextInverse[2] + cipherTextEquiv[3] * plainTextInverse[3];
             for (i = 0; i < key.Count(); i++)
             {
                 key[i] = Mod(key[i], 26);
@@ -97,7 +99,6 @@ namespace SecurityLibrary
                     while (true)
                     {
                         int a = modInverseCnt * det;
-                        Console.WriteLine(a + " " + modInverseCnt);
                         if (a % 26 == 1) break;
                         modInverseCnt++;
                     }
@@ -206,8 +207,78 @@ namespace SecurityLibrary
 
         public List<int> Analyse3By3Key(List<int> plainText, List<int> cipherText)
         {
-            throw new NotImplementedException();
+            List<int> plainTextInverse = new List<int>(new int[9]);
+            int det = 0;
+            //Negative Modulus
+            int Mod(int x, int m)
+            {
+                int r = x % m;
+                return r < 0 ? r + m : r;
+            }
+            //Vertical to Horizontal
+            List<int> plainText2 = new List<int>() { plainText[0], plainText[3], plainText[6], plainText[1], plainText[4], plainText[7], plainText[2], plainText[5], plainText[8] };
+            List<int> cipherText2 = new List<int>() { cipherText[0], cipherText[3], cipherText[6], cipherText[1], cipherText[4], cipherText[7], cipherText[2], cipherText[5], cipherText[8] };
+            //3*3 Matrix Inverse
+            int[,] matrix = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = plainText2[i * 3 + j];
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    plainTextInverse[i * 3 + j] = matrix[(i + 1) % 3, (j + 1) % 3] * matrix[(i + 2) % 3, (j + 2) % 3] -
+                                            matrix[(i + 1) % 3, (j + 2) % 3] * matrix[(i + 2) % 3, (j + 1) % 3];
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                det += matrix[0, i] * plainTextInverse[i];
+            }
+            List<int> temp = new List<int>(new int[plainText2.Count()]);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    temp[j * 3 + i] = plainTextInverse[i * 3 + j];
+                }
+            }
+            plainTextInverse = temp;
+            det = Mod(det, 26);
+            int modInverseCnt = 1;
+            while (true)
+            {
+                int a = modInverseCnt * det;
+                if (a % 26 == 1 || a == 100) break;
+                modInverseCnt++;
+            }
+            det = modInverseCnt;
+            for (int i = 0; i < 3 * 3; i++)
+            {
+                plainTextInverse[i] = Mod(plainTextInverse[i], 26);
+                plainTextInverse[i] *= det;
+                plainTextInverse[i] %= 26;
+            }
+            //Key Calculation
+            List<int> key = new List<int>(new int[9]);
+            key[0] = cipherText2[0] * plainTextInverse[0] + cipherText2[1] * plainTextInverse[3] + cipherText2[2] * plainTextInverse[6];
+            key[1] = cipherText2[0] * plainTextInverse[1] + cipherText2[1] * plainTextInverse[4] + cipherText2[2] * plainTextInverse[7];
+            key[2] = cipherText2[0] * plainTextInverse[2] + cipherText2[1] * plainTextInverse[5] + cipherText2[2] * plainTextInverse[8];
+            key[3] = cipherText2[3] * plainTextInverse[0] + cipherText2[4] * plainTextInverse[3] + cipherText2[5] * plainTextInverse[6];
+            key[4] = cipherText2[3] * plainTextInverse[1] + cipherText2[4] * plainTextInverse[4] + cipherText2[5] * plainTextInverse[7];
+            key[5] = cipherText2[3] * plainTextInverse[2] + cipherText2[4] * plainTextInverse[5] + cipherText2[5] * plainTextInverse[8];
+            key[6] = cipherText2[6] * plainTextInverse[0] + cipherText2[7] * plainTextInverse[3] + cipherText2[8] * plainTextInverse[6];
+            key[7] = cipherText2[6] * plainTextInverse[1] + cipherText2[7] * plainTextInverse[4] + cipherText2[8] * plainTextInverse[7];
+            key[8] = cipherText2[6] * plainTextInverse[2] + cipherText2[7] * plainTextInverse[5] + cipherText2[8] * plainTextInverse[8];
+            for (int i = 0; i < key.Count(); i++)
+            {
+                key[i] = Mod(key[i], 26);
+            }
+            return key;
         }
-
     }
 }
