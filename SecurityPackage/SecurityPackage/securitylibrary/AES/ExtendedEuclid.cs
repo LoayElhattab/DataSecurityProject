03018da0ed367d6df83503348ab61ff41c358e31
kamal-bahaa <kamalbahaa53z@gmail.com>
2025-04-18 23:25:07 +0200

    GetMultiplicativeInverse finished

--- SecurityPackage/SecurityPackage/securitylibrary/AES/ExtendedEuclid.cs
+++ SecurityPackage/SecurityPackage/securitylibrary/AES/ExtendedEuclid.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,7 +17,42 @@ namespace SecurityLibrary.AES
         /// <returns>Mul inverse, -1 if no inv</returns>
         public int GetMultiplicativeInverse(int number, int baseN)
         {
-            throw new NotImplementedException();
+            if (GCD(number, baseN) != 1)
+                return -1;
+
+            int inverse = 0, newInverse = 1;
+            int remainder = baseN, newRemainder = number;
+
+            while (newRemainder != 0)
+            {
+                int quotient = remainder / newRemainder;
+
+                int tempInverse = inverse;
+                inverse = newInverse;
+                newInverse = tempInverse - quotient * newInverse;
+
+                int tempRemainder = remainder;
+                remainder = newRemainder;
+                newRemainder = tempRemainder - quotient * newRemainder;
+            }
+
+            if (inverse < 0)
+                inverse += baseN;
+
+            return inverse;
         }
+
+        public int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
     }
+
 }
